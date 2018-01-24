9c56492517606f04e39a5b5fa02aaf1893123d99
syncWu <sync4814@gmail.com>
2018-01-24 11:15:21 +0800

    isToeplitzmatrix

---
+++ src/ToeplitzMatrix.java
@@ -0,0 +1,20 @@
+// A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+public class ToeplitzMatrix {
+    public static boolean isToeplitzMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[0].length - 1; j++) {
+                if(matrix[i][j] != matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,1,2},{5,4,1}};
+        boolean judge = isToeplitzMatrix(matrix);
+        System.out.println(judge);
+    }
+}
