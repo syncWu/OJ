87244409717d40a55e7e527472104978802453ff
syncWu <sync4814@gmail.com>
2018-01-24 13:07:24 +0800

    SingleNumber

---
+++ src/SingleNumber.java
@@ -0,0 +1,10 @@
+//Given an array of integers, every element appears twice except for one. Find that single one.
+public class SingleNumber {
+    public int singleNumber(int[] nums){
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
